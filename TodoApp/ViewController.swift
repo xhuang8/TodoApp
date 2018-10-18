5117f2d42d0062e9b872770e8f5182d5b50bbc57
xhuang8 <xhuang8@mail.sfsu.edu>
2018-10-18 13:20:54 -0700

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by XiaoQian Huang on 10/18/18.
+//  Copyright © 2018 XiaoQian Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
